7093c2f4c3f079dbe073c80bf98106478ed219c5
Max Kiselyov <m.kiselev@tinkoff.ru>
2021-05-03 23:05:46 +0500

    Initial Commit

---
+++ Start timer WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Start timer WatchKit Extension
+//
+//  Created by m.kiselev on 5/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Start timer WatchKit Extension/Start_timerApp.swift
@@ -0,0 +1,19 @@
+//
+//  Start_timerApp.swift
+//  Start timer WatchKit Extension
+//
+//  Created by m.kiselev on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Start_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
