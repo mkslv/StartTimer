e78b030f4c6c7ebc2ae22e1dd7912b0d1083d0fb
Max Kiselyov <maxkiselyoff@gmail.com>
2021-05-05 00:18:46 +0500

    added model

--- Start timer WatchKit Extension/timer.swift
+++ Start timer WatchKit Extension/timer.swift
@@ -7,3 +7,47 @@
 
 import Foundation
 
+struct TimerModel {
+    var time: Int
+    var timeTitle: String {
+        return "\(time)"
+    }
+    
+    var unit: TimerUnit
+    var unitTitle: String {
+        if time > 1 {return unit.plural()}
+        return unit.rawValue
+    }
+    
+    init(time: Int, unit: TimerUnit) {
+        if time >= 60 {
+            self.time = time / 60
+            self.unit = .min
+        } else {
+            self.time = time
+            self.unit = unit
+        }
+    }
+    
+    static func allTimerValues() -> [TimerModel] {
+        return [
+            TimerModel(time: 3, unit: .min),
+            TimerModel(time: 5, unit: .min)
+        ]
+    }
+    
+    enum TimerUnit: String {
+        case min = "MIN"
+        case sec = "SEC"
+        
+        func plural() -> String {
+            switch self {
+            case .min:
+                return "MINS"
+            case .sec:
+                return "SECS"
+            }
+        }
+    }
+    
+}
